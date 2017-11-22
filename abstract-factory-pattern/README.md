eb029871538ba816771aa7859c666e38a96673fc
Rungen Zhu <cloverzrg@gmail.com>
2017-11-22 16:49:51 +0800

    抽象工厂模式

---
+++ abstract-factory-pattern/README.md
@@ -0,0 +1,4 @@
+# 抽象工厂模式
+
+### 参考
+https://zh.wikipedia.org/wiki/%E6%8A%BD%E8%B1%A1%E5%B7%A5%E5%8E%82
---
+++ abstract-factory-pattern/abstract-factory-pattern.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhurungen
+ * Date: 2017/11/22
+ * Time: 16:16
+ */
+
+//富士康
+abstract class FoxconnFactory
+{
+    abstract public function createButton();
+}
+
+//小米生产线
+class IphoneFactory extends FoxconnFactory
+{
+    public function createButton()
+    {
+        return new IphoneButton();
+    }
+
+
+}
+
+//苹果生产线
+class XiaomiFactory extends FoxconnFactory
+{
+    public function createButton()
+    {
+        return new XiaomiButton();
+    }
+}
+
+class Button {}
+
+//iPhone按钮
+class IphoneButton extends Button
+{
+    public function __construct()
+    {
+        echo '生产iphone按键';
+    }
+}
+
+//小米按钮
+class XiaomiButton extends Button
+{
+    public function __construct()
+    {
+        echo '生产小米按键';
+    }
+}
+
+$phone_type = 'Xiaomi';
+$factory_name = $type.'Factory';
+$factory = new $factory_name;
+$button = $factory->createButton();
